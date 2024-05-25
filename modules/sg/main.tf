23db656c4568cacf6c837cfe9f2a064148926857
Erwin Salas <erwin.salas@tsi.com>
2024-05-24 23:01:25 -0600

    small teawks on SG module

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -1,37 +1,42 @@
 
 locals {
-  # var.ingress_rules is a list of objects. Some rules may have the same name as they belong to the same security group. 
-  # To compute the list of security groups required for the solution, distinct_rule_names generates an array with unique
+  # var.ingress_rules is a list of objects that contains inbound rules for SGs.
+  # Some rules may have the same name as they belong to the same security group. 
+  # To compute the list of security groups required for the solution, sg_groups generates an array with unique
   # security group names by eliminating duplicates.
-  distinct_rule_names = distinct([for rule in var.ingress_rules : rule.name])
-  sg_groups = { for name in local.distinct_rule_names : name => name }
+  sg_groups = distinct([for rule in var.ingress_rules : rule.name])
 }
 
 resource "aws_security_group" "security_group" {
-  for_each = local.sg_groups
-  name        = each.value
+  count       = length(local.sg_groups)
+
+  name        = local.sg_groups[count.index]
   vpc_id      = var.vpc_id
-  description = "security group for ${each.value}"
+  description = "security group for ${local.sg_groups[count.index]}"
 }
 
 resource "aws_security_group_rule" "ingress_rule" {
-  for_each = { for idx, rule in var.ingress_rules : idx => rule }
+  # Two or more inbound rules might have same name so we use index to generate a map 
+  # to simplify data access and manipulation
+  for_each = { for index, rule in var.ingress_rules : index => rule }
 
   type                     = "ingress"
   from_port                = each.value.from_port
   to_port                  = each.value.to_port
   protocol                 = each.value.protocol
   security_group_id        = aws_security_group.security_group[each.value.name].id
+  # There is an exception for external-lb, the inbound rule is driven by cidir block not SG id
   cidr_blocks              = each.value.name != "external-lb" ? null : each.value.cidr_blocks
   source_security_group_id = each.value.name != "external-lb" ?  aws_security_group.security_group[each.value.source_sg].id : null
 }
 
 resource "aws_security_group_rule" "egress_rule" {
-  for_each = local.sg_groups
+  count       = length(local.sg_groups)
 
   type              = "egress"
   from_port   = 0
   to_port     = 0
+  # -1 means we allow all the protocols
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.security_group[each.key].id
